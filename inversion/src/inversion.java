7a62a50a07808d5c07fd59f49c5000c6bd0880e2
VanitLokhande <101892131+VanitLokhande@users.noreply.github.com>
2022-12-06 22:59:13 +0530

    Add files via upload

---
+++ inversion/src/inversion.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class inversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+
+
+
+    }
+}
